d2d8c9e89abcce484d1fb66b2f8ecec258d2f2cd
Jay Han <jaeyeon48@gmail.com>
2022-06-03 15:26:38 +0900

    Testnet change

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,3 +1,18 @@
+// Goerli Test Network
+ 
+// module.exports = {
+//   solidity: {
+//     version: "0.8.4",
+//   }, 
+//   networks: {
+//     goerli: {
+//       url: `https://eth-goerli.alchemyapi.io/v2/${alchemyApiKey}`,
+//       chainId: 5, 
+//       accounts: [privateKey]
+//     },
+//   }
+// };
+
 const { alchemyApiKey, etherscanApiKey, privateKey } = require('./secrets.json');
 
 require("@nomiclabs/hardhat-waffle");
@@ -16,9 +31,9 @@ module.exports = {
     version: "0.8.4",
   }, 
   networks: {
-    goerli: {
-      url: `https://eth-goerli.alchemyapi.io/v2/${alchemyApiKey}`,
-      chainId: 5, 
+    rinkeby: {
+      url: `https://eth-rinkeby.alchemyapi.io/v2/${alchemyApiKey}`,
+      chainId: 4, 
       accounts: [privateKey]
     },
   }
